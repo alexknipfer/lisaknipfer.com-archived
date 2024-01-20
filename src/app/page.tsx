74762533053c4b05a9e06539fc1b80a2834ed5c6
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-20 18:22:08 -0500

    Update global styles and layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Heading from '@/components/heading';
+import PageContent from '@/components/page-content';
+import { ScrollView } from '@/components/scroll-view';
+
 export default function Home() {
   return (
-    <div>
-      <h1>Lisas Website</h1>
-    </div>
+    <ScrollView>
+      <Heading level="h2">About Me</Heading>
+      <PageContent>
+        <p>This is the about me section</p>
+      </PageContent>
+    </ScrollView>
   );
 }
