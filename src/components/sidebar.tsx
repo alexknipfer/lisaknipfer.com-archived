d05496315cb6f7e49fb8ab5146138a93e6eb42c5
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-20 21:34:06 -0500

    Increase sidebar width on larger screen sizes

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -31,10 +31,10 @@ export default function Sidebar() {
   });
 
   return (
-    <div className="w-60 bg-zinc-50 border-r border-zinc-200 shrink-0 p-3 text-sm">
+    <div className="w-60 shrink-0 border-r border-zinc-200 bg-zinc-50 p-3 text-sm xl:w-72">
       <Link
         href="/"
-        className="flex gap-3 hover:bg-pink-100 transition-colors duration-200 p-2 rounded mb-3"
+        className="mb-3 flex gap-3 rounded p-2 transition-colors duration-200 hover:bg-pink-100"
       >
         <Avatar>
           <AvatarImage src="/assets/avatar.jpg" />
@@ -55,22 +55,22 @@ export default function Sidebar() {
               key={template}
               href={template}
               className={cn(
-                'transition duration-200 p-2 rounded-lg flex justify-between items-center text-pink-800',
+                'flex items-center justify-between rounded-lg p-2 text-pink-800 transition duration-200',
                 {
                   'hover:bg-pink-100': !isCurrentPath,
                   'bg-pink-950 text-zinc-50': isCurrentPath,
                 },
               )}
             >
-              <span className="flex gap-2 items-center">
+              <span className="flex items-center gap-2">
                 <Icon size={16} />
                 <span className="font-medium">{label}</span>
               </span>
               <span
                 className={cn(
-                  'flex justify-center items-center w-5 h-5 rounded bg-zinc-100 border border-zinc-200 text-xs',
+                  'flex h-5 w-5 items-center justify-center rounded border border-zinc-200 bg-zinc-100 text-xs',
                   {
-                    'text-zinc-50 bg-zinc-600 border-zinc-500': isCurrentPath,
+                    'border-zinc-500 bg-zinc-600 text-zinc-50': isCurrentPath,
                   },
                 )}
               >
