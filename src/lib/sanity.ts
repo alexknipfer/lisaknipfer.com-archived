c100709969c1336224c8b48aeb72ca0300b5c439
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-21 20:40:26 -0500

    Add get page by id

--- src/lib/sanity.ts
+++ src/lib/sanity.ts
@@ -2,7 +2,7 @@ import 'server-only';
 import { createClient, QueryParams, SanityClient } from 'next-sanity';
 
 import { appConfig } from '@/config/app-config';
-import { SanityPage } from '@/types/sanity-page.interface';
+import { SanityPage, SanityPageWithBuilder } from '@/types/sanity';
 
 export class Sanity {
   private client: SanityClient;
@@ -30,6 +30,36 @@ export class Sanity {
     });
   }
 
+  public getPageById(id: string) {
+    return this.sanityFetch<SanityPageWithBuilder>({
+      query: `
+        *[_type == 'page' && _id == $id][0] {
+          title,
+          ${this.pageBuilderQuery}
+        }
+      `,
+      params: {
+        id,
+      },
+      tags: ['page'],
+    });
+  }
+
+  public getPageBySlug(slug: string) {
+    return this.sanityFetch<SanityPageWithBuilder>({
+      query: `
+        *[_type == 'page' && slug.current == $slug][0] {
+          title,
+          ${this.pageBuilderQuery}
+        }
+      `,
+      params: {
+        slug,
+      },
+      tags: ['page'],
+    });
+  }
+
   private async sanityFetch<QueryResponse>({
     query,
     params = {},
@@ -45,6 +75,23 @@ export class Sanity {
         tags,
       },
     });
+  }
+
+  private get pageBuilderQuery() {
+    return `
+      pageBuilder[]{
+        _type == 'pageDescription' => {
+          _type,
+          title,
+          description
+        },
+        _type == 'timeline' => @-> {
+          _type,
+          name,
+          timelineYears | order(year desc)
+        }
+      }
+    `;
   }
 }
 
