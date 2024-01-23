8bca5ceaa2e8039d19708b9f9297fe1a14616fc4
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-22 21:45:43 -0500

    Update type to include timeline items

--- src/types/sanity.ts
+++ src/types/sanity.ts
@@ -1,8 +1,16 @@
 import type dynamicIconImports from 'lucide-react/dynamicIconImports';
 
-enum PageContentBuilderType {
+export enum PageContentBuilderType {
   PAGE_DESCRIPTION = 'pageDescription',
   TIMELINE = 'timeline',
+  TIMELINE_YEAR = 'timelineYear',
+  TIMELINE_ITEM = 'timelineItem',
+}
+
+export type PageContentType = PageDescription | Timeline;
+
+interface SanityArrayItemBase {
+  _key: string;
 }
 
 export interface SanityPageWithBuilder extends SanityPage {
@@ -22,9 +30,22 @@ export interface PageDescription {
   description: string;
 }
 
+interface TimelineItem extends SanityArrayItemBase {
+  _type: PageContentBuilderType.TIMELINE_ITEM;
+  name: string;
+  description: string;
+}
+
+interface TimelineYear extends SanityArrayItemBase {
+  _type: PageContentBuilderType.TIMELINE_YEAR;
+  year: string;
+  timelineItems: Array<TimelineItem>;
+}
+
 export interface Timeline {
   _type: PageContentBuilderType.TIMELINE;
   name: string;
+  timelineYears: Array<TimelineYear>;
 }
 
 export interface Slug {
