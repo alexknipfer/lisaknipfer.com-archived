3f72f16819d48bf80705d84d4f1116e3dee1c286
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-24 23:32:10 -0500

    Revalidate path

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,4 +1,4 @@
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { type NextRequest, NextResponse } from 'next/server';
 import { parseBody } from 'next-sanity/webhook';
 
@@ -23,11 +23,13 @@ export async function POST(req: NextRequest) {
       return new Response(JSON.stringify({ message, body }), { status: 400 });
     }
 
-    console.info('REVALIDATE TAG: ', body._type);
-    revalidateTag(body._type);
+    // console.info('REVALIDATE TAG: ', body._type);
+    // revalidateTag(body._type);
     if (body.slug) {
-      console.info('REVALIDATE TAG: ', `${body._type}:${body.slug}`);
-      revalidateTag(`${body._type}:${body.slug}`);
+      // console.info('REVALIDATE TAG: ', `${body._type}:${body.slug}`);
+      // revalidateTag(`${body._type}:${body.slug}`);
+      console.log('REVALIDATE PATH: ', `/${body.slug}`);
+      revalidatePath(`/${body.slug}`);
     }
 
     return NextResponse.json({ body });
