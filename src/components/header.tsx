aa9125ada8af062b43c0a7caead59e52da696796
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-27 16:46:02 -0500

    Remove menu items from header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,12 +1,9 @@
-import { sanity } from '@/lib/sanity';
 import { MobileDrawer } from './mobile-drawer';
 
 export async function Header() {
-  const { menuItems } = await sanity.getSettings();
-
   return (
     <header className="supports-backdrop-blur:bg-white/8- sticky inset-0 z-50 flex h-12 w-full items-center border-b border-zinc-200 bg-white bg-white/80 px-4 backdrop-blur lg:hidden">
-      <MobileDrawer menuItems={menuItems} />
+      <MobileDrawer />
     </header>
   );
 }
