6bb90ddbe751d70a223a9c6bdcc2bd63bd396ba8
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-26 23:34:17 -0500

    Update layout to have content max width

--- src/components/page-content.tsx
+++ src/components/page-content.tsx
@@ -1,5 +1,5 @@
 import { PropsWithChildren } from 'react';
 
 export function PageContent({ children }: PropsWithChildren) {
-  return <div className="mt-3">{children}</div>;
+  return <div className="mt-3 max-w-screen-md">{children}</div>;
 }
