9df184183febfb2c7f2854b1e2bd01b9f8ee2f23
Alex <alexanderknipfer@gmail.com>
2024-01-29 18:13:34 -0500

    Revert "TASK: Use t3 env and separate sanity queries from client"

--- src/app/(personal)/page.tsx
+++ src/app/(personal)/page.tsx
@@ -3,7 +3,7 @@ import { PageContent } from '@/components/page-content';
 import { PageWrapper } from '@/components/page-wrapper';
 import { ScrollView } from '@/components/scroll-view';
 import { PageBuilder } from '@/components/page-builder';
-import { sanity } from '@/lib/sanity.client';
+import { sanity } from '@/lib/sanity';
 import { Header } from '@/components/header';
 
 export default async function Home() {
