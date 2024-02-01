8052af9b15ee9e850ebfbcae89c3a34742485129
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-31 20:42:32 -0500

    Decrease padding on mobile and add scroll bottom padding

--- src/components/scroll-view.tsx
+++ src/components/scroll-view.tsx
@@ -2,7 +2,7 @@ import { PropsWithChildren } from 'react';
 
 export function ScrollView({ children }: PropsWithChildren) {
   return (
-    <div className="h-full max-h-dvh min-h-dvh w-full overflow-y-auto">
+    <div className="h-full max-h-dvh min-h-dvh w-full overflow-y-auto pb-5">
       {children}
     </div>
   );
