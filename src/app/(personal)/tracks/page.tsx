6a33ab2eeffbc55d0ec31996c15c7c9bb3d8cf9e
Alexander Knipfer <alexanderknipfer@gmail.com>
2024-01-31 20:42:45 -0500

    Add recently played spotify tracks

--- src/app/(personal)/tracks/page.tsx
+++ src/app/(personal)/tracks/page.tsx
@@ -1,16 +1,15 @@
+import { Suspense } from 'react';
+
 import { Heading } from '@/components/heading';
 import { PageContent } from '@/components/page-content';
 import { PageWrapper } from '@/components/page-wrapper';
 import { ScrollView } from '@/components/scroll-view';
 import { Header } from '@/components/header';
-import { spotify } from '@/lib/spotify.client';
 import { sanity } from '@/lib/sanity.client';
+import { RecentlyPlayedTracks } from './components/recently-played-tracks';
 
 export default async function Tracks() {
-  const [recentlyPlayed, page] = await Promise.all([
-    spotify.getRecentlyPlayed(),
-    sanity.getPageBySlug('tracks'),
-  ]);
+  const page = await sanity.getPageBySlug('tracks');
 
   return (
     <ScrollView>
@@ -20,11 +19,9 @@ export default async function Tracks() {
           {page.title}
         </Heading>
         <PageContent>
-          {recentlyPlayed.items.map(({ track }) => (
-            <div key={track.id} className="mb-5">
-              {track.name}
-            </div>
-          ))}
+          <Suspense>
+            <RecentlyPlayedTracks />
+          </Suspense>
         </PageContent>
       </PageWrapper>
     </ScrollView>
